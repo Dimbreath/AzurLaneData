286a41ef1d3bb3bee083c409472b60754040c833
Server <dimbreath-server@gmail.com>
2019-12-24 08:00:36 +0000

    [EN] 3.1.120

--- EN/sharecfg/ship_skin_template.lua
+++ EN/sharecfg/ship_skin_template.lua
@@ -329,6 +329,109 @@ pg.ship_skin_template = {
 			}
 		}
 	},
+	[101022] = {
+		ship_group = 10102,
+		name = "冬夜的感谢",
+		bullet_skin_main = "",
+		desc = "列克星敦小姐，把这身圣诞节风格的衣服送给了我，说这是作为她的粉丝的礼物。指挥官，觉得好看吗？",
+		illustrator2 = -1,
+		id = 101022,
+		bullet_skin_secondary = "",
+		no_showing = "1",
+		painting = "duwei_3",
+		air_bullet_skin = "",
+		shop_type_id = 1,
+		hand_id = 5,
+		bgm = "",
+		lip_smoothing = 0,
+		l2d_animations = "",
+		bg = "125",
+		skin_type = 0,
+		bg_sp = "",
+		voice_actor_2 = -1,
+		air_torpedo_skin = "",
+		prefab = "duwei_3",
+		l2d_se = "",
+		aircraft_skin = "",
+		main_UI_FX = "",
+		shop_id = 70282,
+		voice_actor = 178,
+		air_bomb_skin = "",
+		illustrator = -1,
+		rarity_bg = "",
+		l2d_para_range = "",
+		lip_sync_gain = 0,
+		show_skin = "stand",
+		l2d_voice_calibrate = "",
+		tag = {
+			4
+		},
+		live2d_offset = {
+			0,
+			0,
+			0
+		},
+		fx_container = {
+			{
+				0,
+				1.99185,
+				1.15
+			},
+			{
+				0,
+				0,
+				0
+			},
+			{
+				0,
+				0.75,
+				-1.299
+			},
+			{
+				0,
+				0,
+				0
+			}
+		},
+		bound_bone = {
+			cannon = {
+				{
+					1.207,
+					1.131,
+					0
+				}
+			},
+			torpedo = {
+				{
+					-0.008,
+					0,
+					0
+				}
+			},
+			antiaircraft = {
+				{
+					1.17,
+					1.161,
+					0
+				}
+			}
+		},
+		smoke = {
+			{
+				50,
+				{
+					{
+						"smoke",
+						{
+							-0.45,
+							2.37,
+							-0.16
+						}
+					}
+				}
+			}
+		}
+	},
 	[101030] = {
 		ship_group = 10103,
 		name = "Cassin",
@@ -86004,6 +86107,7 @@ pg.ship_skin_template = {
 		100000,
 		100010,
 		101020,
+		101022,
 		101030,
 		101039,
 		101040,
