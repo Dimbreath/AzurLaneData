72ca7690834024da0c1a4b32d3c544bb099b4a76
Dimbreath-Server <dimbreath+1@gmail.com>
2020-01-02 02:53:53 +0000

    [TW] 3.2.131

--- TW/gamecfg/story/fuyingyinghua16.lua
+++ TW/gamecfg/story/fuyingyinghua16.lua
@@ -9,7 +9,7 @@ return {
 			expression = 3,
 			side = 2,
 			dir = 1,
-			bgm = "holo-ayame",
+			bgm = "xinnong-2",
 			actor = 302210,
 			nameColor = "#a9f548",
 			say = "斩------！",
@@ -44,7 +44,7 @@ return {
 			side = 2,
 			dir = 1,
 			actor = 305140,
-			say = "虽然模拟战斗了无数次，亲眼见到之后，压迫力完全不是一个等级啊",
+			say = "（虽然模拟战斗了无数次，亲眼见到之后，压迫力完全不是一个等级啊——！！）",
 			typewriter = {
 				speed = 0.05,
 				speedUp = 0.01
@@ -71,12 +71,12 @@ return {
 			}
 		},
 		{
-			expression = 1,
-			nameColor = "#a9f548",
+			expression = 4,
 			side = 2,
 			dir = 1,
 			actor = 305140,
-			say = "哈哈哈…没想到第一次实战机会是出现在这种情况下",
+			nameColor = "#a9f548",
+			say = "（没想到第一次实战机会是出现在这种情况下！！果然当初就不该接下这祭典代行者的任务啊！）",
 			typewriter = {
 				speed = 0.05,
 				speedUp = 0.01
@@ -84,34 +84,26 @@ return {
 			painting = {
 				alpha = 0.3,
 				time = 1
+			},
+			action = {
+				{
+					y = 0,
+					type = "shake",
+					delay = 0,
+					dur = 0.3,
+					x = 20,
+					number = 2
+				}
 			}
 		},
 		{
 			expression = 3,
 			side = 2,
-			bgName = "bg_xinnong_cg4",
-			dir = 1,
-			actorName = "{namecode:179}",
 			nameColor = "#a9f548",
-			hidePaintObj = true,
+			dir = 1,
+			actor = 306070,
+			actorName = "{namecode:179}",
 			say = "在后方平和的环境中待久了，总会让人产生一丝和平时代的错觉…",
-			flashout = {
-				dur = 0.5,
-				black = true,
-				alpha = {
-					0,
-					1
-				}
-			},
-			flashin = {
-				delay = 0.5,
-				dur = 0.5,
-				black = true,
-				alpha = {
-					1,
-					0
-				}
-			},
 			typewriter = {
 				speed = 0.05,
 				speedUp = 0.01
@@ -124,11 +116,10 @@ return {
 		{
 			expression = 4,
 			side = 2,
-			bgName = "bg_xinnong_cg4",
-			actorName = "{namecode:179}",
-			dir = 1,
 			nameColor = "#a9f548",
-			hidePaintObj = true,
+			dir = 1,
+			actor = 306070,
+			actorName = "{namecode:179}",
 			say = "塞壬的威胁其实一刻都没有消失！",
 			typewriter = {
 				speed = 0.05,
@@ -142,11 +133,10 @@ return {
 		{
 			expression = 4,
 			side = 2,
-			bgName = "bg_xinnong_cg4",
-			actorName = "{namecode:179}",
-			dir = 1,
 			nameColor = "#a9f548",
-			hidePaintObj = true,
+			dir = 1,
+			actor = 306070,
+			actorName = "{namecode:179}",
 			say = "诸位，难得的实战机会，尽情的展示所学，尽情的战斗吧！",
 			typewriter = {
 				speed = 0.05,
@@ -155,6 +145,15 @@ return {
 			painting = {
 				alpha = 0.3,
 				time = 1
+			},
+			action = {
+				{
+					y = 30,
+					type = "shake",
+					delay = 0,
+					dur = 0.15,
+					number = 2
+				}
 			}
 		}
 	}
