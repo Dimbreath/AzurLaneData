9a4d2941ff43146fa66390b440520a71ee2cf46f
Dimbreath-Server <dimbreath+1@gmail.com>
2020-02-27 02:20:32 +0000

    [ko-KR] Hash: 3.0.260 | CV: 150 | L2D: 158

--- ko-KR/model/proxy/servernoticeproxy.lua
+++ ko-KR/model/proxy/servernoticeproxy.lua
@@ -14,6 +14,18 @@ function slot0.register(slot0)
 			table.insert(slot0.data, ServerNotice.New(slot5))
 		end
 
+		if HXSet.isHx() and pg.SdkMgr.GetInstance():GetChannelUID() == "0" then
+			table.insert(slot0.data, ServerNotice.New({
+				btn_title = "공지사항",
+				version = "special_age_12_dfhdkfjk",
+				title_image = "https://blcdn.imtxwy.com/notice/gonggao_kr2.jpg",
+				content = "존경하는 유저 여러분, 구글 측의 정책 요구에 의하여, \"벽람항로T\"는 2020년 2월 27일부로 Google Play 스토어에서 서비스가 중단되며, 해당 어플리케이션은 다운로드가 제공되지 않습니다. App Store와 Google Play 스토어 상의 \"벽람항로\" 어플리케이션에 대해서는 영향이 없습니다. 서비스 중단 후에도 이미 다운로드한 \"벽람항로T\"를 사용하여 게임 플레이가 가능하지만, 후속 버전의 업데이트에 대해서는 영향을 받게 됩니다.앞으로의 업데이트 내용을 정상적으로 체험하는데 도움을 드리기 위하여, 계정 데이터는 보류될 것입니다. 가급적 빠른 기간 내로 게임 계정을 Facebook 또는 휴대폰 번호와 연동하시길 권해드리며, 해당 경우, IOS 및 안드로이드의 \"벽람항로\" 어플리케이션에서 정상적으로 로그인할 수 있습니다.google play 스토어에서 \"벽람항로\"를 검색하고 18+ 버전을 다운로드하여 게임을 플레이해주시기 바랍니다.이로 인해 불편을 끼쳐드린 점, 깊은 사과의 말씀을 드립니다.",
+				id = 999999,
+				title = "\"벽람항로T\" 구글 스토어 서비스 중단 안내",
+				time_desc = "2020/2/25"
+			}))
+		end
+
 		slot0:sendNotification(slot1.SERVER_NOTICES_UPDATE)
 	end)
 end
