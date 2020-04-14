549cc9786f4ceadf6cd183f95693888ec7e31bd9
Dimbreath-Server <dimbreath+1@gmail.com>
2020-04-14 11:45:37 +0000

    [en-US] Hash: 3.1.354 | CV: 114 | L2D: 149

--- en-US/gamecfg/guide/newguide/segments/s006.lua
+++ en-US/gamecfg/guide/newguide/segments/s006.lua
@@ -64,8 +64,8 @@ return {
 				pathIndex = -1,
 				path = "/LevelCamera/Canvas/UIMain/LevelGrid/DragLayer/plane/cells/chapter_cell_4_6/attachment",
 				pos = {
-					x = 828.3,
-					y = 69.2
+					x = 63.1,
+					y = 67.7
 				},
 				triggerType = {
 					1
